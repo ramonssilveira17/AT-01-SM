7752091af8833dbecd9edb69996eb6be2d96bf39
ramonssilveira17 <swakegot@gmail.com>
2020-08-26 21:58:45 -0300

    Adicionando projeto react-native

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+Icon.loadFont()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itenscoluna}>
+        <Icon name="person" size={40}></Icon>
+        <Text>Perfil</Text>
+      </View>
+
+      <View style={styles.itenscoluna}>
+        <Icon name="camera-alt" size={40}></Icon>
+        <Text>Câmera</Text>
+      </View>
+
+      <View style={styles.itenscoluna}>
+        <Icon name="exit-to-app" size={40}></Icon>
+        <Text>Sair</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +27,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bdbdbd',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 100,
+  },
+  views: {
+    width: 50,
+    height: 50,
+    margin: 10,
+  },
+  itenscoluna: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginTop: 15,
+    width: '100%',
+    height: '100%',
+    borderRadius: 5,
+    elevation: 15,
   },
 });
